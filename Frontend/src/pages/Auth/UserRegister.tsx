db6ff564d444438b599a9d596d7debcb59697b70
Ravi <raviaa912@gmail.com>
2026-06-06 14:08:46 +0530

    feat(user-register): initialed user register

--- Frontend/src/pages/Auth/UserRegister.tsx
+++ Frontend/src/pages/Auth/UserRegister.tsx
@@ -64,6 +64,7 @@ const UserRegister = () => {
 
 
     const handleGoogleAuth=async ()=>{
+        //import from .env
         window.location.href ="http://localhost:4000/google/auth";
         
         return true;
